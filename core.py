46c07e0e2ec9aee453510b5b26af46838374a33f
THargreaves <tim.hargreaves@icloud.com>
2020-07-18 22:10:17 +0100

    Add method for creating matrix from rows

--- core.py
+++ core.py
@@ -31,6 +31,13 @@ class Matrix(ABC):
         """Abstract class property for the zero element of a field."""
         pass
 
+    @property
+    @staticmethod
+    @abstractmethod
+    def _type():
+        """Abstract class property for the type of matrix elements."""
+        pass
+
     @staticmethod
     def validate_dimensions(m, n):
         """Check whether a pair of matrix dimensions are valid."""
@@ -38,6 +45,43 @@ class Matrix(ABC):
             raise TypeError("dimensions must be integral")
         if m <= 0 or n <= 0:
             raise ValueError("dimensions must be positive")
+
+    @classmethod
+    def from_rows(cls, rows):
+        """Make a matrix from a list of rows."""
+        m = len(rows)
+        n = len(rows[0])
+        # Check that rows are consistent lengths
+        if any([len(row) != n for row in rows[1:]]):
+            raise ValueError("inconsistent row lengths")
+        # Dispatch generic function if class is not specified
+        if cls is Matrix:
+            return Matrix._from_rows_guess_type(m, n, rows)
+        # Check that data types match class type
+        t = cls._type
+        if any(any(type(e) is not t for e in r) for r in rows):
+            raise TypeError("element types don't match class type")
+        return cls(m, n, rows)
+
+    @staticmethod
+    def _from_rows_guess_type(m, n, rows):
+        """Make a matrix from a list of rows."""
+        # Check that data types are consistent
+        t = type(rows[0][0])
+        if any(any(type(e) is not t for e in r[(i == 0):])
+               for i, r in enumerate(rows)):
+            raise TypeError("inconsistent element types")
+        # Dispatch to children based on type
+        if t is bool:
+            return BooleanMatrix(m, n, rows)
+        elif t is int:
+            return IntegerMatrix(m, n, rows)
+        elif t is float:
+            return RealMatrix(m, n, rows)
+        else:
+            raise NotImplementedError(
+                "no class implemented for the given element type"
+            )
 
     @classmethod
     def zeros(cls, m, n):
@@ -67,6 +111,7 @@ class BooleanMatrix(Matrix):
 
     _zero = False
     _one = True
+    _type = bool
 
 
 class NumericMatrix(Matrix, ABC):
@@ -80,6 +125,7 @@ class IntegerMatrix(NumericMatrix):
 
     _zero = 0
     _one = 1
+    _type = int
 
 
 class RationalMatrix(NumericMatrix):
@@ -97,6 +143,7 @@ class RealMatrix(NumericMatrix):
 
     _zero = 0.
     _one = 1.
+    _type = float
 
 
 class ComplexMatrix(NumericMatrix):
