0afc8cdb3b8769e910cf36963188897642a6ec64
THargreaves <tim.hargreaves@icloud.com>
2020-07-19 10:47:30 +0100

    Fix bug in __init__.py

--- mytrix/__init__.py
+++ mytrix/__init__.py
@@ -1,7 +1,7 @@
 """A Python package for linear algebra focused on the mathematical user."""
 
-from core import Matrix, Vector  # noqa
-from core import BooleanMatrix, BooleanVector  # noqa
-from core import NumericMatrix, NumericVector  # noqa
-from core import IntegerMatrix, IntegerVector  # noqa
-from core import RealMatrix, RealVector  # noqa
+from .core import Matrix, Vector  # noqa
+from .core import BooleanMatrix, BooleanVector  # noqa
+from .core import NumericMatrix, NumericVector  # noqa
+from .core import IntegerMatrix, IntegerVector  # noqa
+from .core import RealMatrix, RealVector  # noqa
